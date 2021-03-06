df29d03630591d939776277a76b151e3a26f4edc
Paul Robson <paul@robsons.org.uk>
2021-03-06 20:13:38 +0000

    Added READ. Nearly at 0.01

--- basic/demo.bas
+++ basic/demo.bas
@@ -1,5 +1,13 @@
+data "Paul"
 print "START"
+dim c(5),name$(2)
+read a,b2,c(3)
+print a,b2,c(3)
+for i = 1 to 2:read name$(i):next i
+for i = 1 to 2:print name$(i):next i
 data 12,13,14:print "NEXT"
+print "TEXTIES"
+data "Paul","Jane"
 print "DONE"
 repeat:until false
 
--- source/main/commands/readdata.asm
+++ source/main/commands/readdata.asm
@@ -10,15 +10,49 @@
 ; ************************************************************************************************
 
 		.section storage
-
+;
+;		Current position. Should rest on seperating , if not search forward for data.
+;
 dataPtr:									; codePointer and index for Data.
 		.fill 	2
-dataIndex:
+dataIndex:									; current position.
 		.fill 	1
 
 		.send storage
 
 		.section code
+
+; ************************************************************************************************
+;
+;										Command READ
+;
+; ************************************************************************************************
+
+CommandRead:	;; [read]
+		ldx 	#0 							; get a reference to a variable in stack:0		
+		jsr 	EvaluateReference
+		jsr 	SwapDPCP 					; use the data pointer.
+		lda 	(codePtr),y 				; see where it is.
+		iny
+		cmp 	#TKW_COMMA 					; if it is on a comma, it is in data.
+		beq 	_CRInData
+		;
+		dey
+		lda 	#TKW_DATA 					; scan forward to a DATA statement.
+		tax
+		jsr 	ScanForward
+_CRInData:
+		ldx 	#1 							; evaluate a value at Stack,1
+		jsr 	Evaluate 		
+		dex 								; and write the value.
+		jsr 	WriteValue
+		jsr 	SwapDPCP 					; get the code pointer back.
+		lda 	(codePtr),y 				; what follows ?
+		iny
+		cmp 	#TKW_COMMA 					; if comma, another variable
+		beq 	CommandRead
+		dey
+		rts
 
 ; ************************************************************************************************
 ;
@@ -47,4 +81,27 @@ CommandRestore: ;; [restore]
 		sta 	dataIndex
 		rts
 
+; ************************************************************************************************
+;
+;						Swap DataPointer/DataIndex and CodePtr/Y
+;
+; ************************************************************************************************
+
+SwapDPCP:
+		.pshx
+		tya 								; swap Y, DataIndex
+		ldy 	DataIndex
+		sta 	DataIndex
+		;
+		lda 	codePtr 					; swap code/dataptr low
+		ldx 	dataptr
+		sta 	dataPtr
+		stx 	codePtr
+
+		lda 	codePtr+1 					; swap code/dataptr high.
+		ldx 	dataptr+1
+		sta 	dataPtr+1
+		stx 	codePtr+1
+		.pulx
+		rts
 		.send code
