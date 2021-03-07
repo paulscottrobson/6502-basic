18ce480b05f1576d89737a3918e609d37f7756fa
Paul Robson <paul@robsons.org.uk>
2021-03-07 08:33:36 +0000

    Array test

--- scripts/tests/testarr.py
+++ scripts/tests/testarr.py
@@ -1,9 +1,9 @@
 # *****************************************************************************
 # *****************************************************************************
 #
-#		Name:		testvar.py
+#		Name:		testarr.py
 #		Author:		Paul Robson (paul@robsons.org.uk)
-#		Date:		1st March 2021
+#		Date:		7th March 2021
 #		Purpose:	Test assignment and variables.
 #
 # *****************************************************************************
@@ -16,28 +16,33 @@ class AssignTest(Test):
 
 	def prefixCode(self,h):
 		self.current = {}
-		for i in range(1,40):
+		for i in range(1,20):
 			name = "".join([chr(random.randint(97,117)) for c in range(0,random.randint(1,5))])
 			if len(name) > 1:
 				name = name[0]+"0"+name[1:]
 			if i % 2 == 0:
 				name = name + "$"
+			name += "("
+			size = random.randint(1,9)			
 			if name not in self.current:
-				self.current[name] = None
+				self.current[name] = [ None ] * size
+				h.write("dim {0}{1})\n".format(name,size))
 		self.names = [x for x in self.current.keys()]
 
 	def getTest(self,n):
-		var = self.names[random.randint(0,len(self.names)-1)]
+		var = self.names[random.randint(0,len(self.names)-1)]		
+		index = random.randint(0,len(self.current[var])-1)
 		value = str(random.randint(0,999))
-		if var.endswith("$"):
+		if var.find("$") >= 0:
 			value = '"'+"".join([chr(random.randint(65,85)) for s in range(0,random.randint(0,7))])+'"'
-		self.current[var] = value
-		return "{0} = {1}".format(var,value)
+		self.current[var][index] = value
+		return "{0}{2}) = {1}".format(var,value,index)
 
 	def postfixCode(self,h):
 		for n in self.names:
-			if self.current[n] is not None:
-				h.write("assert {0} = {1}\n".format(n,self.current[n]))
+			for i in range(0,len(self.current[n])):
+				if self.current[n][i] is not None:
+					h.write("assert {0}{2}) = {1}\n".format(n,self.current[n][i],i))
 
 if __name__ == "__main__":		
 	t = AssignTest(500)
